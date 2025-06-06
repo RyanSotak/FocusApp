e1f52995f266071760d07dddb0bd2c0e309e65e1
Ryan <ryancheese011@gmail.com>
2025-06-06 15:38:51 +0200

    I don't think it can be called in the background

--- android/app/src/main/kotlin/com/focus/app_of_focus/MainActivity.kt
+++ android/app/src/main/kotlin/com/focus/app_of_focus/MainActivity.kt
@@ -1,5 +1,93 @@
 package com.focus.app_of_focus
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import android.app.ActivityManager
+import android.app.usage.UsageStatsManager
+import android.app.usage.UsageStats
+import android.provider.Settings
+import android.os.Build
 
-class MainActivity : FlutterActivity()
+
+
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "Battery"
+
+    private fun getRunningAppProcesses(): List<String> {
+        val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        return activityManager.runningAppProcesses
+            ?.map { it.processName }
+            ?: emptyList()
+    }
+
+    private fun getForegroundApp(): String? {
+        val usageStatsManager = getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+        val endTime = System.currentTimeMillis()
+        val beginTime = endTime - 10000 // Look back 10 seconds
+
+        val usageStatsList: List<UsageStats> = usageStatsManager.queryUsageStats(
+            UsageStatsManager.INTERVAL_DAILY, beginTime, endTime)
+
+        if (usageStatsList.isNullOrEmpty()) {
+            return null
+        }
+
+        // Find the app with the most recent last time used
+        val recentStat = usageStatsList.maxByOrNull { it.lastTimeUsed }
+        return recentStat?.packageName
+    }
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            // This method is invoked on the main thread.
+//                call, result ->
+//            if (call.method == "getBatteryLevel") {
+//                val batteryLevel = getBatteryLevel()
+//
+//                if (batteryLevel != -1) {
+//                    result.success(batteryLevel)
+//                } else {
+//                    result.error("UNAVAILABLE", "Battery level not available.", null)
+//                }
+//            } else {
+//                result.notImplemented()
+//            }
+
+                call, result ->
+            if (call.method == "getRunningApps") {
+//            val processes = getRunningAppProcesses()
+//            result.success(processes)
+//                startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
+                val processes = getForegroundApp()
+                result.success(processes)
+
+            }
+            else {
+                result.notImplemented()
+            }
+
+        }
+    }
+    private fun getBatteryLevel(): Int {
+        val batteryLevel: Int
+        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+            val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        } else {
+            val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+            batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+        }
+
+        return batteryLevel
+    }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,53 @@ import 'package:flutter/services.dart';
 import 'dart:isolate';
 import 'package:permission_handler/permission_handler.dart' as permission_handler;
 import 'package:permission_handler/permission_handler.dart';
+import 'dart:async';
+
+const MethodChannel channel = MethodChannel('Battery');
 
 
 
+class MyTaskHandler extends TaskHandler {
+  @override
+  Future<void> onStart(DateTime timestamp, TaskStarter starter) async {
+    // Optional: Initialization logic
+  }
+
+  Future<void> _getBatteryLevel() async {
+    String processList;
+    try {
+      final processes = await channel.invokeMethod<String>('getRunningApps');
+      //processList = 'Processes: ${processes?.join(', ') ?? "None"}';
+      processList = processes.toString();
+      if (kDebugMode) {
+        debugPrint(processList);
+      }
+    } on PlatformException catch (e) {
+      processList = "Failed to get processes: '${e.message}'.";
+    }
+    // setState(() {
+    //   _batteryLevel = processList;
+    // });
+    // setState(() {
+    //   _batteryLevel = batteryLevel;
+    // });
+  }
+
+  @override
+  void onRepeatEvent(DateTime timestamp) {
+    _getBatteryLevel();
+  }
+
+  @override
+  Future<void> onDestroy(DateTime timestamp, bool isTimeout) async {}
+  @override
+  void onNotificationButtonPressed(String id) {}
+  @override
+  void onNotificationPressed() {}
+}
 
 void foregroundTaskCallback() async {
-  const channel = MethodChannel('app_status');
+  const channel = MethodChannel('Battery');
   try {
     final isRunning = await channel.invokeMethod('isAppInForeground', {
       'packageName': 'com.whatsapp', // Replace with the package you want to check
@@ -57,19 +98,138 @@ void main() {
 //   // Register your custom plugin.
 //   AppStatusPlugin.registerWith();
 // }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Navigation Demo',
-      home: const HomeScreen(),
-
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  //static const platform = MethodChannel('Battery');
+  // String _batteryLevel = 'Unknown battery level.';
+
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Home')),
+
+        body: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton(
+                  onPressed: openUsageAccessSettings,
+                  child: const Text('Grant Usage Access'),
+                ),
+                const SizedBox(height: 16), // spacing between buttons
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const SecondScreen()),
+                    );
+                  },
+                  child: const Text('Go to App selection menu!'),
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: startForegroundService,
+                  child: const Text('Start Monitoring'),
+                ),
+              ],
+            )
+        )
+    );
+  }
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Material(
+  //     child: Center(
+  //       child: Column(
+  //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  //         children: [
+  //           ElevatedButton(
+  //             onPressed: _getBatteryLevel,
+  //             child: const Text('Get Battery Level'),
+  //           ),
+  //           Text(_batteryLevel),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
+}
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Navigation Demo',
+//       home: const HomeScreen(),
+//
+//     );
+//   }
+// }
 
 Future<void> startForegroundService() async {
   if (Platform.isAndroid && await Permission.notification.isDenied) {
@@ -113,43 +273,43 @@ Future<void> stopForegroundService() async {
   await FlutterForegroundTask.stopService();
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton(
-                onPressed: openUsageAccessSettings,
-                child: const Text('Grant Usage Access'),
-              ),
-              const SizedBox(height: 16), // spacing between buttons
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const SecondScreen()),
-                  );
-                },
-                child: const Text('Go to App selection menu!'),
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: startForegroundService,
-                child: const Text('Start Monitoring'),
-              ),
-            ],
-          )
-        )
-      );
-    }
-  }
+// class HomeScreen extends StatelessWidget {
+//   const HomeScreen({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text('Home')),
+//
+//         body: Center(
+//           child: Column(
+//             mainAxisSize: MainAxisSize.min,
+//             children: [
+//               ElevatedButton(
+//                 onPressed: openUsageAccessSettings,
+//                 child: const Text('Grant Usage Access'),
+//               ),
+//               const SizedBox(height: 16), // spacing between buttons
+//               ElevatedButton(
+//                 onPressed: () {
+//                   Navigator.push(
+//                     context,
+//                     MaterialPageRoute(builder: (context) => const SecondScreen()),
+//                   );
+//                 },
+//                 child: const Text('Go to App selection menu!'),
+//               ),
+//               const SizedBox(height: 16),
+//               ElevatedButton(
+//                 onPressed: startForegroundService,
+//                 child: const Text('Start Monitoring'),
+//               ),
+//             ],
+//           )
+//         )
+//       );
+//     }
+//   }
 
 
 
@@ -209,45 +369,53 @@ void callbackDispatcher() {
   FlutterForegroundTask.setTaskHandler(MyTaskHandler());
 }
 
-class MyTaskHandler extends TaskHandler {
-  @override
-  Future<void> onStart(DateTime timestamp, TaskStarter starter) async {
-    if (kDebugMode) {
-      debugPrint('[MyTaskHandler] Started at $timestamp');
-    }
-    // Use starter.sendPort to communicate, if needed
-  }
-
-  @override
-  void onRepeatEvent(DateTime timestamp) {
-    // This now returns void and takes only DateTime
-    const channel = MethodChannel('app_status');
-    channel.invokeMethod('isAppInForeground', {
-      'packageName': 'com.whatsapp',
-    }).then((isRunning) {
-      if (kDebugMode) {
-        debugPrint('App in foreground: $isRunning');
-      }
-    }).catchError((e) {
-      if (kDebugMode) {
-        debugPrint('Error: $e');
-      }
-    });
-  }
-
-  @override
-  Future<void> onDestroy(DateTime timestamp, bool isTimeout) async {
-    print('[MyTaskHandler] Destroyed at $timestamp, isTimeout: $isTimeout');
-  }
-
-  @override
-  void onNotificationButtonPressed(String id) {
-    print('[MyTaskHandler] Button pressed: $id');
-  }
-
-  @override
-  void onNotificationPressed() {
-    print('[MyTaskHandler] Notification pressed');
-  }
-}
+// class MyTaskHandler extends TaskHandler {
+//   @override
+//   Future<void> onStart(DateTime timestamp, TaskStarter starter) async {
+//     if (kDebugMode) {
+//       debugPrint('[MyTaskHandler] Started at $timestamp');
+//     }
+//     // Use starter.sendPort to communicate, if needed
+//   }
+//
+//   Future<void> _getBatteryLevel() async {
+//     String processList;
+//     try {
+//       final processes = await platform.invokeMethod<String>('getRunningApps');
+//       //processList = 'Processes: ${processes?.join(', ') ?? "None"}';
+//       processList = processes.toString();
+//       if (kDebugMode) {
+//         debugPrint(processList);
+//       }
+//     } on PlatformException catch (e) {
+//       processList = "Failed to get processes: '${e.message}'.";
+//     }
+//     // setState(() {
+//     //   _batteryLevel = processList;
+//     // });
+//     // setState(() {
+//     //   _batteryLevel = batteryLevel;
+//     // });
+//   }
+//
+//   @override
+//   void onRepeatEvent(DateTime timestamp) {
+//     _getBatteryLevel();
+//   }
+//
+//   @override
+//   Future<void> onDestroy(DateTime timestamp, bool isTimeout) async {
+//     print('[MyTaskHandler] Destroyed at $timestamp, isTimeout: $isTimeout');
+//   }
+//
+//   @override
+//   void onNotificationButtonPressed(String id) {
+//     print('[MyTaskHandler] Button pressed: $id');
+//   }
+//
+//   @override
+//   void onNotificationPressed() {
+//     print('[MyTaskHandler] Notification pressed');
+//   }
+// }
 
